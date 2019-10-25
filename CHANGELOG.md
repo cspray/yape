59a8ba6a5fd51737dc34216f4efe0b217ecf066d
Charles <cspray@users.noreply.github.com>
2019-10-25 05:17:10 -0400

    Document polish for 2.1 release (#15)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,9 +2,12 @@
 
 All notable changes to this project will be documented in this file.
 
-The format is based on [Keep a Changelog] and this project adheres to [Semantic Versioning]
+The format is based on [Keep a Changelog] and this project adheres to [Semantic Versioning]. The changes in this file 
+include those that may be part of the user-facing API as well as the developer-facing API. For example, changes to the 
+Enum interface or CLI commands impact the user of this library while changes to the components under the `Cspray\Yape\Internal` 
+namespace are intended for developers and maintainers working on YAPE itself.
 
-## 2.1.0 - 2019-10-??
+## 2.1.0 - 2019-10-25
 
 This version represents some significant refactoring for how YAPE works internally. However, this did not alter the 
 public API available through the Symfony console in a breaking manner nor did it alter the enum code that was generated. 
--- src/EnumTrait.php
+++ src/EnumTrait.php
@@ -3,8 +3,10 @@
 namespace Cspray\Yape;
 
 use Cspray\Yape\Exception\InvalidArgumentException;
+use http\Exception\BadQueryStringException;
 
 /**
+ * A reusable implementation of the methods and intent required by the Enum interface.
  *
  * @package Cspray\Yape
  * @license See LICENSE in source root
@@ -15,19 +17,44 @@ trait EnumTrait {
 
     private $enumValue;
 
+    /**
+     * If this method is overridden it is critical that setEnumValue is called appropriately.
+     *
+     * @param string $enumValue
+     */
     private function __construct(string $enumValue) {
         $this->setEnumValue($enumValue);
     }
 
+    /**
+     * Sets the value of the Enum, which corresponds to its toString value and the string necessary to return the instance
+     * from valueOf.
+     *
+     * This should be called 1 time from within the constructor on instance creation.
+     *
+     * @param string $enumValue
+     */
     private function setEnumValue(string $enumValue) : void {
         $this->enumValue = $enumValue;
     }
 
+    /**
+     * Returns an array of Enum instances that could be instantiated by this Enum.
+     *
+     * @return self[]
+     */
     static public function values() : array {
         self::primeContainer();
         return array_values(self::$container);
     }
 
+    /**
+     * Return an Enum instance that matches the given $name or throw an InvalidArgumentException if the $Name is not a
+     * valid value for this Enum.
+     *
+     * @param string $name
+     * @return static
+     */
     static public function valueOf(string $name) : self {
         self::primeContainer();
         if (!isset(self::$container[$name])) {
@@ -37,14 +64,34 @@ trait EnumTrait {
         return self::$container[$name];
     }
 
+    /**
+     * Returns whether or not this Enum is equal to $compare.
+     *
+     * @param $compare
+     * @return bool
+     */
     public function equals($compare) : bool {
         return $this === $compare;
     }
 
+    /**
+     * Returns the value passed to setEnumValue.
+     *
+     * @return string
+     */
     public function toString() : string {
         return $this->enumValue;
     }
 
+    /**
+     * The factory method that static method constructors should use when creating their instances.
+     *
+     * This method guarantees that for each $enumValue only one instance will be created.
+     *
+     * @param string $enumValue
+     * @param mixed ...$constructorArgs
+     * @return static
+     */
     static protected function getSingleton(string $enumValue, ...$constructorArgs) : self {
         if (!isset(self::$container[$enumValue])) {
             self::$container[$enumValue] = new self($enumValue, ...$constructorArgs);
@@ -53,6 +100,10 @@ trait EnumTrait {
         return self::$container[$enumValue];
     }
 
+    /**
+     * Ensures that all values possible for this Enum are instantiated; primarily used to ensure that the values() and
+     * valueOf() methods work appropriately.
+     */
     static private function primeContainer() {
         $allowedValues = self::getAllowedValues();
         if (count(self::$container) !== count($allowedValues)) {
@@ -62,6 +113,11 @@ trait EnumTrait {
         }
     }
 
+    /**
+     * Return an array of enum values, that correspond to static method constructors, that are allowed for this Enum.
+     *
+     * @return string[]
+     */
     static abstract protected function getAllowedValues() : array;
 
 }
